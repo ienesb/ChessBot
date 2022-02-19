05a47772109500ba904afeaf133fceffda188e57
ienesb <ienes9625@gmail.com>
2022-02-19 22:58:24 +0300

    game constructor

--- src/game.cpp
+++ src/game.cpp
@@ -5,7 +5,54 @@
 #include "pieces.h"
 
 Game::Game() {
-
+    int i,j;
+    for(i = 0; i < 8; i++){
+        for(j = 0; j < 8; j++){
+            this->board[8*i+j] = new Block(nullptr, this, j+1, 8-i);
+        }
+    }
+    for(i = 1; i < 9; i++){
+        Pawn* wPawn = new Pawn("w", this->getBlock(i, 2), this);
+        Pawn* bPawn = new Pawn("b", this->getBlock(i, 7), this);
+        this->getBlock(i, 2))->setPiece(wPawn);
+        this->getBlock(i, 7))->setPiece(bPawn);
+        switch(i){
+            case 1:
+            case 8:
+                Rook* wRook = new Rook("w", this->getBlock(i, 1), this);
+                Rook* bRook = new Rook("b", this->getBlock(i, 8), this);
+                this->getBlock(i, 1))->setPiece(wRook);
+                this->getBlock(i, 8))->setPiece(bRook);
+                break;
+            case 2:
+            case 7:
+                Knight* wKnight = new Knight("w", this->getBlock(i, 1), this);
+                Knight* bKnight = new Knight("b", this->getBlock(i, 8), this);
+                this->getBlock(i, 1))->setPiece(wKnight);
+                this->getBlock(i, 8))->setPiece(bKnight);
+                break;
+            case 3:
+            case 6:
+                Bishop* wBishop = new Bishop("w", this->getBlock(i, 1), this);
+                Bishop* bBishop = new Bishop("b", this->getBlock(i, 8), this);
+                this->getBlock(i, 1))->setPiece(wBishop);
+                this->getBlock(i, 8))->setPiece(bBishop);
+                break;
+            case 4:
+                Queen* wQueen = new Queen("w", this->getBlock(i, 1), this);
+                this->bKing = new King("b", this->getBlock(i, 8), this);
+                this->getBlock(i, 1))->setPiece(wQueen);
+                this->getBlock(i, 8))->setPiece(bKing);
+                break;
+            case 5:
+                this->wKing = new King("w", this->getBlock(i, 1), this);
+                Queen* bQueen = new Queen("b", this->getBlock(i, 8), this);
+                this->getBlock(i, 1))->setPiece(wKing);
+                this->getBlock(i, 8))->setPiece(bQueen);
+                break;
+        }
+    }
+    this->update();
 }
 
 void Game::press(Block *pressed) {
