091c8f62415e50528bc73d55f0942e73b54b847d
ienesb <ienes9625@gmail.com>
2022-03-20 17:55:31 +0300

    bot model initial

--- src/bot.cpp
+++ src/bot.cpp
@@ -5,10 +5,31 @@
 #include "move.h"
 
 Bot::Bot(std::string color, Game* game, std::string modelPath):Player::Player(color, game){
-
+    // torch::load(model, modelPath);
 }
 
 std::vector<Move> Bot::listAllMoves(){
     // type here
     // use game pointer and pieces vector
 }
+
+// Move Bot::getBestMove(std::vector<Move> moves){
+//     Move m, bestMove;
+//     int result = 0;
+//     int prediction;
+//     for(m: moves){
+//         prediction = this->model->forward(encode(m));
+//         if(prediction > result){
+//             result = prediction;
+//             bestMove = m;
+//         }
+//     }
+//     return bestMove;
+// }
+
+// int Bot::encode(Move move){
+//     this->game->performMovement(move);
+//     int result = getBinaryBoard(this->game);
+//     this->game->undo();
+//     return result;
+// }
